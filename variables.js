4e62fe17b14f6e067342e92367da8689a9adb4ef
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-08-03 10:32:28 +0100

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,3 +3,16 @@ let totalmoney = 4000;
 let moneypaidsofar = 2348;
 let lefttobepaid = totalmoney - moneypaidsofar;
 console.log(`the total bill is £${totalmoney} the remaining to be paid is £${lefttobepaid}`);
+
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
