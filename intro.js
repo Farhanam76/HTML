e49580c3a360cf7e580d85344d6189fab949e2e2
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-08-04 12:01:32 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,9 @@
+ console.log("Farhana");
+console.info("my surname is motlib");
+console.warn("I am a sagittarius");
+console.error("location is confidential")
+let car = "mercedes"
+let model = "e-class"
+console.log("my favourite care is " + car +"and the model is" +model)
+console.log(`My favorite car is ${car} and the model is ${model}.`);
+onsole.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
