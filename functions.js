a991c3f829c093c6ae7da7052566f013726aa4fc
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-08-04 12:24:23 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,26 @@
+//1)
+
+function subtraction (num1,num2) {
+    return num1 - num2
+}
+console.log(subtraction(20,5));
+
+//2
+
+
+const welcome = function(name, age, gender) {
+    return (` My name is ${name}, I am ${age}, I am gender of ${gender} `)
+    
+}
+console.log(welcome("Farhana",22,"female"));
+
+//3)
+powerup = (n1,n2) =>{
+    return maths.pow(n1,n2);
+
+}
+onst welcome = function(name, age, gender) {
+    return (` My name is ${name}, I am ${age}, I am gender of ${gender} `)
+    
+}
+console.log(welcome(Farhana,22,female));
