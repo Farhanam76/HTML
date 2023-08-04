640746766581bb0430170e7a1bc5075c254293b5
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-08-04 12:00:23 +0100

    Create day4.js

---
+++ day4.js
@@ -0,0 +1,5 @@
+//variables exercise
+let totalmoney = 4000;
+let moneypaidsofar = 2348;
+let lefttobepaid = totalmoney - moneypaidsofar;
+console.log(`the total bill is £${totalmoney} the remaining to be paid is £${lefttobepaid}`);
