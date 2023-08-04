e2f5892f92c9697ecc6b5e4640f27125888aa66f
Farhanam76 <138291154+Farhanam76@users.noreply.github.com>
2023-08-04 11:59:06 +0100

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,33 @@
+/1)
+let A = 100;
+while (A >= 200) {
+    console.log(A);
+    A++;
+}
+
+//2)
+let A = 100;
+while (A >= 200) {
+    if (A%2 == 0);
+    {console.log("-");}
+else{
+
+
+console.log("*")}
+A++
+}
+
+//3
+for (let i = 0; i < 10; i++) {
+    for (let n = 1; n < 11; n++)
+    console.log(n);
+}
+
+//4
+for (let a = 100; a <= 200; a++) {
+    console.log(a)
+    
+}
+
+
+
